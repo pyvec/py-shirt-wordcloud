6f038ab61f27aac24358c96b90c31476b8c5cd79
Petr Viktorin <encukou@gmail.com>
2016-09-27 15:43:01 +0200

    Two-part wordcloud

--- make_wordcloud.py
+++ make_wordcloud.py
@@ -2,6 +2,8 @@ import re
 import collections
 import os
 import subprocess
+import unicodedata
+import random
 
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 import numpy as np
@@ -9,6 +11,9 @@ from PIL import Image
 from scipy.misc import imread
 
 coloring = np.array(Image.open("logo.png"))
+blue_img = np.array(Image.open("logo-blue.png"))
+yellow_img = np.array(Image.open("logo-yellow.png"))
+background_img = np.array(Image.open("logo-background.png"))
 
 more_stopwords = {'na', 'se', '38a', 'www', 'je', 'co', 'si', 'cz', 'po', 'že',
                   'od', 'než', 'nás', 'tak', 'na', 'za', 'pro', 'ale', 'už',
@@ -18,8 +23,41 @@ more_stopwords = {'na', 'se', '38a', 'www', 'je', 'co', 'si', 'cz', 'po', 'že',
                   'přes', 'nich', 'což', 'není', 'jsi', 'měla', 'jako', 'jsou',
                   'máš', 'musí', 'tam', 'ti', 'pod', 'ní', 'tomu', 'tu', 'kdy',
                   'jsem', 'atd', 'byli', 'své', 'má', 'https', 'ho', 'jpg',
-                  'budu', 'jsme', 'images', 'měl', 'byla', 'jen', 'min'}
+                  'budu', 'jsme', 'images', 'měl', 'byla', 'jen', 'min', 'ale',
+                  'ne', 'nic', 'toho', 'mě', 'html'}
 STOPWORDS = STOPWORDS.union(more_stopwords)
+
+class Term:
+    def __init__(self):
+        self.variants = collections.Counter()
+        self.contexts = collections.Counter()
+        self.contexts[random.choice(['blog', 'pyvo'])] += 0.001
+
+    def add(self, word, context, n=1):
+        self.variants[word] += n
+        self.contexts[context] += n
+
+    @classmethod
+    def normalize(cls, name):
+        normalized = unicodedata.normalize('NFKD', name.lower())
+        return re.sub('[^a-z]', '', normalized)
+
+    @property
+    def word(self):
+        return self.variants.most_common(1)[0][0]
+
+    @property
+    def occurences(self):
+        return sum(self.contexts.values())
+
+    @property
+    def context(self):
+        for context, n in self.contexts.most_common():
+            if context != 'materialy':
+                return context
+
+
+term_dict = {}
 
 def text_to_frequencies(text):
     words = text
@@ -49,18 +87,27 @@ def text_to_frequencies(text):
     counter['Praha'] = counter['Brno'] = counter['Ostrava'] = 0
     return counter
 
-counter = collections.Counter()
+def add_text(text, context, bias=1):
+    counter = text_to_frequencies(text)
+    for word, n in counter.items():
+        key = Term.normalize(word)
+        term = term_dict.setdefault(key, Term())
+        term.add(word, context, n * bias)
+
+def frequencies_for_context(context=None):
+    return list((term.word, term.occurences) for term in term_dict.values()
+                if context is None or term.context == context)
 
 for name in 'pyvo-brno.txt', 'pyvo-praha.txt', 'pyvo-ostrava.txt':
     with open(name, 'r') as f:
-        counter += text_to_frequencies(f.read())
+        add_text(f.read(), 'pyvo')
 
 for dirpath, dirnames, filenames in os.walk('./pyladies.cz/original/v1'):
     for filename in filenames:
         if filename.endswith('.html'):
             fullname = os.path.join(dirpath, filename)
             out = subprocess.check_output(['links', '-dump', fullname])
-            counter += text_to_frequencies(out.decode('utf-8'))
+            add_text(out.decode('utf-8'), 'materialy')
 
 BAD_POSTS = (
     # bez diakritiky :( 
@@ -77,25 +124,73 @@ for dirpath, dirnames, filenames in os.walk('./blog.python.cz/content'):
             with open(fullname) as f:
                 while f.readline().strip():
                     "skip block of declarations before first newline"
-                counter += text_to_frequencies(f.read())
+                add_text(f.read(), 'blog')
 
 
-print(len(counter), 'words')
+print(len(term_dict), 'terms')
+for i in range(8):
+    nam, term = term_dict.popitem()
+    print(nam, term, term.word, term.occurences, term.context, term.variants, term.contexts)
+for ctx in set(t.context for t in term_dict.values()):
+    print(ctx, len(frequencies_for_context(ctx)))
 
-wordcloud = WordCloud(
-                      #font_path='flux.ttf',
-                      font_path='/home/pviktori/.fonts/gf/BreeSerif-Regular.ttf',
-                      background_color='black',
-                      width=1372,
-                      height=1372,
-                      prefer_horizontal=0.8,
-                      max_words=500,
-                      relative_scaling=0.5,
-                      scale=7,
-                      mask=coloring,
-                     ).generate_from_frequencies(counter.items())
 
-image_colors = ImageColorGenerator(coloring)
+for context in set(t.context for t in term_dict.values()) | {'materialy'}:
+    total = sum(t.contexts.get(context, 0) for t in term_dict.values())
+    if context == 'materialy':
+        total /= 2
+    print(context, total)
+    for term in term_dict.values():
+        if context in term.contexts:
+            term.contexts[context] /= total
 
-wordcloud.recolor(color_func=image_colors)
-wordcloud.to_file("wordcloud.png")
+
+default_random = random.Random()
+
+def color_func(word, font_size, position, orientation, font_path, random_state):
+    if random_state is None:
+        random_state = default_random
+
+    mini = 0
+    maxi = 255
+    saturation = 80
+    lightness = 50
+    term = term_dict[Term.normalize(word)]
+    context = term.context
+    if context == 'blog':
+        hue = 207
+        saturation = 80
+        lightness = 65
+    elif context == 'pyvo':
+        hue = 46
+        saturation = 70
+        lightness = 50
+    elif context == 'materialy-xx':
+        mini = 0
+        maxi = 5
+        return "rgb(255, 255, 255)"
+    return "hsl({}, {}%, {}%)".format(int(round(hue)),
+                                      int(round(saturation)),
+                                      int(round(lightness)))
+
+
+def make_wordcloud(context, img):
+    print('Making wordcloud:', context)
+    return WordCloud(
+        #font_path='flux.ttf',
+        font_path='/home/pviktori/.fonts/gf/BreeSerif-Regular.ttf',
+        background_color='black',
+        width=1372,
+        height=1372,
+        prefer_horizontal=0.8,
+        max_words=500,
+        relative_scaling=0.5,
+        scale=7,
+        color_func=color_func,
+        mask=img,
+       ).generate_from_frequencies(frequencies_for_context(context))
+
+array = (make_wordcloud('blog', blue_img).to_array() +
+         make_wordcloud('pyvo', yellow_img).to_array())
+
+Image.fromarray(array).save("wordcloud.png")
